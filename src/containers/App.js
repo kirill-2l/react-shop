38106c763b0dda11705efd1f4c10e904a5da51c5
Kirill <petrovvkirill@gmail.com>
2020-02-06 23:02:01 +0300

    added filter and search among items

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,23 +1,19 @@
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as booksActions from "../actions/books";
+import * as filterActions from "../actions/filter";
 import orderBy from "lodash/orderBy";
 
 import App from "../components/App";
 
-const sortBy = (books, filterBy, searchQuery) => {
-  books = books.filter(
-    o =>
-      o.title.toLowerCase().indexOf(searchQuery.toLowerCase) ||
-      o.author.toLowerCase().indexOf(searchQuery.toLowerCase)
-  );
+const sortBy = (books, filterBy) => {
   switch (filterBy) {
     case "all":
       return books;
     case "price_high":
-      return orderBy(books, "price", "desc");
-    case "price_low":
       return orderBy(books, "price", "asc");
+    case "price_low":
+      return orderBy(books, "price", "desc");
     case "author":
       return orderBy(books, "author", "asc");
     default:
@@ -25,13 +21,21 @@ const sortBy = (books, filterBy, searchQuery) => {
   }
 };
 
+const filterBooks = (books, searchQuery) =>
+  books.filter(
+    o =>
+      o.title.toLowerCase().indexOf(searchQuery.toLowerCase()) >= 0 ||
+      o.author.toLowerCase().indexOf(searchQuery.toLowerCase()) >= 0
+  );
+const searchBooks = (books, filterBy, searchQuery) => {
+  return sortBy(filterBooks(books, searchQuery), filterBy)
+}
 const mapStateToProps = ({ books, filter }) => ({
-  books: books.item && sortBy(books.items, books.filterBy, filter.searchQuery),
+  books: books.items && searchBooks(books.items, filter.filterBy, filter.searchQuery),
   isReady: books.isReady
 });
 
 const mapDispatchToProps = dispatch => ({
   ...bindActionCreators(booksActions, dispatch)
 });
-
 export default connect(mapStateToProps, mapDispatchToProps)(App);
